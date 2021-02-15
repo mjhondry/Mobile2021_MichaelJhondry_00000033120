183344554d7798cc24b143a6632dca3dc186f34e
powered5126 <64387638+powered5126@users.noreply.github.com>
2021-02-15 19:18:31 +0800

    00000033120_Week03

---
+++ 33120_Week03/33120_Week03/app/src/main/java/umn/ac/id/MainActivity.java
@@ -0,0 +1,85 @@
+package umn.ac.id;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import android.view.Menu;
+
+import android.view.MenuItem;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private RecyclerView mRecyclerView;
+    private DaftarKataAdapter mAdapter;
+
+    private final LinkedList<String> mDaftarKata = new LinkedList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+
+                int jumlahKata = mDaftarKata.size();
+                mDaftarKata.addLast("Kata " + (jumlahKata + 1) + " telah ditambahkan");
+
+                Objects.requireNonNull(mRecyclerView.getAdapter()).notifyItemInserted(jumlahKata);
+                mRecyclerView.smoothScrollToPosition(jumlahKata);
+            }
+        });
+
+        for(int i = 1; i < 21; i++)
+        {
+
+            mDaftarKata.add("Kata " + i);
+            mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+            mAdapter = new DaftarKataAdapter(this, mDaftarKata);
+            mRecyclerView.setAdapter(mAdapter);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
