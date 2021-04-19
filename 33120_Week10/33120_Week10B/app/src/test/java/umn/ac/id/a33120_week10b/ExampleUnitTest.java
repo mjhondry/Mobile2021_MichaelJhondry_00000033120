7207fc5c3c56281920d7610d864379e3634d53a9
Michael Jhondry <brem9090@gmail.com>
2021-04-19 13:01:48 +0800

    00000033120_Week10

---
+++ 33120_Week10/33120_Week10B/app/src/test/java/umn/ac/id/a33120_week10b/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package umn.ac.id.a33120_week10b;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
